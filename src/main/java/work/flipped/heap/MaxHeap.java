79c7faeee0a35b6268c2058e356763cbcbad9ca7
wangrui <1123630646@qq.com>
2020-02-12 00:08:36 +0800

    获取最大元素和删除元素

--- src/main/java/work/flipped/heap/MaxHeap.java
+++ src/main/java/work/flipped/heap/MaxHeap.java
@@ -55,15 +55,67 @@ public class MaxHeap<E extends Comparable<E>> {
         return index * 2 + 2;
     }
 
+    /**
+     * 添加元素
+     *
+     * @param e
+     */
     public void add(E e) {
         data.addLast(e);
         siftUp(data.getSize() - 1); // 上浮
     }
 
+    /**
+     * 上浮
+     *
+     * @param i
+     */
     private void siftUp(int i) {
         while (i > 0 && data.get(i).compareTo(data.get(i)) < 0) {
             data.swap(i, (parent(i)));
             i = parent(i);
+        }
+    }
+
+    /**
+     * 查看最大元素
+     *
+     * @return
+     */
+    public E findMax() {
+        if (data.getSize() == 0) {
+            throw new IllegalArgumentException("heap is empty");
+        }
+        return data.get(0);
+    }
+
+    /**
+     * 取出最大元素
+     */
+    public E remove() {
+        E ret = findMax();
+        data.swap(0, data.getSize() - 1);
+        data.removeLast();
+        siftDown(0);
+        return ret;
+    }
+
+    /**
+     * 下沉
+     *
+     * @param i
+     */
+    private void siftDown(int i) {
+        while (left(i) < data.getSize()) {
+            int j = left(i);
+            if (j + 1 < data.getSize() && data.get(j + 1).compareTo(data.get(j)) > 0) {
+                j = right(i);
+            }
+            if (data.get(i).compareTo(data.get(j)) >= 0) {
+                break;
+            }
+            data.swap(i, j);
+            i = j;
         }
     }
 
